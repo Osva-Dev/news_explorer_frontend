9bca8c5f53b09d4fedaf4987c387be6edb522769
Osva-Dev <osvadev30@gmail.com>
2026-05-25 12:05:28 -0600

    feat: add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header__container">
+      <h1 className="headerTitle">NewsExplorer</h1>
+      <p>Inicio</p>
+      <button>Iniciar Sesión</button>
+    </div>
+  );
+}
+
+export default Header;
