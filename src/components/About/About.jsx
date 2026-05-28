44d08daf02a6bfe9a6451be697060930b8ecef08
Osva-Dev <osvadev30@gmail.com>
2026-05-28 16:55:49 -0600

    styles: update styles from the component about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,15 +4,32 @@ import MajinVegeta from "../../images/majin_vegeta.jpg";
 
 function About() {
   return (
-    <div className="about__container">
+    <div className="about">
       <img className="about__image" src={MajinVegeta} alt="Profile Picture" />
-      <h2 className="about__title">Acerca del Autor</h2>
-      <p className="about__paragraph">
-        Este bloque describe al autor del proyecto. Aquí debe indicar tu nombre,
-        a qué te dedicas y qué tecnologías de desarrollo conoces. También puedes
-        hablar de tu experiencia con Practicum, de lo que aprendiste allí y de
-        cómo puedes ayudar a los clientes potenciales.
-      </p>
+      <div className="about__info">
+        <h2 className="about__title">Acerca del Autor</h2>
+        <p className="about__paragraph">
+          Hola a todos, soy Osvaldo Ochoa, Desarrollador Web Jr. Actualmente me
+          encuentro especializándome en el Desarrollo Web, con enfoque en
+          frontend utilizando React, así como en backend con Node.js y Express.
+          Además, trabajo con bases de datos MongoDB para desarrollar
+          aplicaciones web modernas, dinámicas y escalables. Me apasiona seguir
+          aprendiendo y mejorar constantemente mis habilidades para crear
+          experiencias funcionales y atractivas.
+        </p>
+        <p className="about__paragraph">
+          Mi formación ha estado acompañada por mi experiencia en TripleTen,
+          donde he trabajado en proyectos enfocados en buenas prácticas de
+          programación, desarrollo frontend y construcción de aplicaciones full
+          stack. Durante este proceso he fortalecido habilidades en React,
+          JavaScript, Node.js, Express y MongoDB, además de aprender
+          metodologías de trabajo similares a las utilizadas en entornos
+          profesionales reales. Gracias a esta preparación, puedo ayudar a
+          clientes potenciales a crear sitios y aplicaciones web modernas,
+          responsivas y funcionales, priorizando tanto la experiencia del
+          usuario como un código limpio y escalable.
+        </p>
+      </div>
     </div>
   );
 }
