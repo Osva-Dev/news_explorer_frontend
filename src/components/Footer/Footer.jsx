920d0ced2fe1a7c6560be08ebf427477324d5374
Osva-Dev <osvadev30@gmail.com>
2026-05-28 17:40:49 -0600

    styles: update styles from the component footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,15 +5,19 @@ import Github from "../../assets/Icons/github.png";
 
 function Footer() {
   return (
-    <footer className="footer__container">
+    <footer className="footer">
       <p className="footer__copyright">
         {"\u00A9"} 2020 Supersite, Powered by News API
       </p>
       <div className="footer__info">
-        <p>Inicio</p>
-        <p>Practicum</p>
-        <img className="footer__icon" src={Github} alt="Github Icon" />
-        <img className="footer__icon" src={Facebook} alt="Facebook Icon" />
+        <div className="footer__links">
+          <p className="footer__link">Inicio</p>
+          <p className="footer__link">Practicum</p>
+        </div>
+        <div className="footer__icons">
+          <img className="footer__icon" src={Github} alt="Github Icon" />
+          <img className="footer__icon" src={Facebook} alt="Facebook Icon" />
+        </div>
       </div>
     </footer>
   );
