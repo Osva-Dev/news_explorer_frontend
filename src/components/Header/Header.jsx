0e7139a96388e6daf17a62077e83bd575cdedc96
Osva-Dev <osvadev30@gmail.com>
2026-06-01 16:51:32 -0600

    fix: update the navigation component structure and its respective styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,21 +5,10 @@ import { Link } from "react-router-dom";
 import SearchForm from "../SearchForm/SearchForm";
 import Navigation from "../Navigation/Navigation";
 
-import newExplorer from "/newsexplorer.svg";
-
 function Header() {
   return (
     <header className="header">
-      <div className="header__container">
-        <Link to="/">
-          <img
-            className="header__logo"
-            src={newExplorer}
-            alt="News Explorer Logo"
-          />
-        </Link>
-        <Navigation />
-      </div>
+      <Navigation />
       <div className="header__hero">
         <h1 className="header__title">¿Qué está pasando en el mundo?</h1>
         <p className="header__paragraph">
