41abeb41f04106894f7118bbada5d95cf331760c
Osva-Dev <osvadev30@gmail.com>
2026-06-11 17:51:00 -0600

    styles: add tablet responsive styles to about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,13 +9,11 @@ function About() {
       <div className="about__info">
         <h2 className="about__title">Acerca del Autor</h2>
         <p className="about__paragraph">
-          Hola a todos, soy Osvaldo Ochoa, Desarrollador Web Jr. Actualmente me
-          encuentro especializándome en el Desarrollo Web, con enfoque en
-          frontend utilizando React, así como en backend con Node.js y Express.
-          Además, trabajo con bases de datos MongoDB para desarrollar
-          aplicaciones web modernas, dinámicas y escalables. Me apasiona seguir
-          aprendiendo y mejorar constantemente mis habilidades para crear
-          experiencias funcionales y atractivas.
+          Hola soy Osvaldo Ochoa, Desarrollador Web Jr. Actualmente me encuentro
+          especializándome en el Desarrollo Web, con enfoque en frontend
+          utilizando React, así como en backend con Node.js y Express. Además,
+          trabajo con bases de datos MongoDB para desarrollar aplicaciones web
+          modernas, dinámicas y escalables.
         </p>
         <p className="about__paragraph">
           Mi formación ha estado acompañada por mi experiencia en TripleTen,
@@ -24,10 +22,7 @@ function About() {
           stack. Durante este proceso he fortalecido habilidades en React,
           JavaScript, Node.js, Express y MongoDB, además de aprender
           metodologías de trabajo similares a las utilizadas en entornos
-          profesionales reales. Gracias a esta preparación, puedo ayudar a
-          clientes potenciales a crear sitios y aplicaciones web modernas,
-          responsivas y funcionales, priorizando tanto la experiencia del
-          usuario como un código limpio y escalable.
+          profesionales reales.
         </p>
       </div>
     </div>
